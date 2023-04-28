603622b1c234ac254e9e74a194964c473141174f
kernel-dev <twitchisadeck@gmail.com>
2023-04-28 21:24:53 +0200

    add(ShipScript): filter rooms to corresponding lists

--- Assets/Scripts/ShipScript.cs
+++ Assets/Scripts/ShipScript.cs
@@ -4,49 +4,86 @@ using UnityEngine;
 
 public class ShipScript : MonoBehaviour
 {
-    [SerializeField] private int id;
-    [SerializeField] private float flight_height;
-    [SerializeField] private float flight_width;
-    [SerializeField] private float shield_height;
-    [SerializeField] private float shield_width;
-    [SerializeField] private GameObject shield_prefab;
-    private GameObject ship_shield;
-    [SerializeField] private int max_hp;
-    private float hp;
-    [SerializeField] private GameObject health_bar;
-    [SerializeField] private int max_shield;
-    private float shield_hp;
-    [SerializeField] private GameObject shield_bar;
-    private float dodge;
+    [SerializeField] private int        id;
 
-    private static GameObject[] ship_list;
-    private GameObject[] room_list;
-    private List<GameObject> missiles_inflight;
-    private List<GameObject> craft_inflight;
-    // Start is called before the first frame update
+    [SerializeField] private float      flight_height;
+    [SerializeField] private float      flight_width;
+    [SerializeField] private float      shield_height;
+    [SerializeField] private float      shield_width;
+    [SerializeField] private GameObject shield_prefab;
+                     private GameObject ship_shield;
+    [SerializeField] private GameObject shield_bar;
+                     private float      shield_hp;
+    [SerializeField] private int        max_shield;
+
+    [SerializeField] private int        max_hp;
+                     private float      hp;
+    [SerializeField] private GameObject health_bar;
+                     private float      dodge;
+
+    private static GameObject[]         ship_list;
+    private GameObject[]                room_list;
+    private List<GameObject>            missiles_inflight;
+    private List<GameObject>            craft_inflight;
+
+    protected List<GameObject>          hangars;
+    protected List<GameObject>          missiles;
+    protected List<GameObject>          lasers;
+    protected List<GameObject>          reactors;
+    protected List<GameObject>          engines;
+    protected List<GameObject>          point_defenses;
+    protected List<GameObject>          shields;
+
+    //
+    //  Start is called before
+    //  the first frame update.
+    //
     void Start()
     {
         registerSelf();
+
         hp = max_hp;
         health_bar.GetComponent<StatBarScript>().renderBar(max_hp);
+
         shield_hp = max_shield;
         shield_bar.GetComponent<StatBarScript>().renderBar(max_shield);
-        missiles_inflight = new List<GameObject>();
-        craft_inflight = new List<GameObject>();
+
+        missiles_inflight   = new List<GameObject>();
+        craft_inflight      = new List<GameObject>();
+        
+        //
+        //  Instantiate all the room lists.
+        //
+        hangars         = new List<GameObject>();
+        missiles        = new List<GameObject>();
+        lasers          = new List<GameObject>();
+        engines         = new List<GameObject>();
+        reactors        = new List<GameObject>();
+        engines         = new List<GameObject>();
+        point_defenses  = new List<GameObject>();
+        shields         = new List<GameObject>();
     }
 
-    // Update is called once per frame
+    //
+    //  Update is called
+    //  once per frame.
+    //
     void Update()
     {
         if(shield_hp > 0 && ship_shield == null)
         {
             ship_shield = GameObject.Instantiate(shield_prefab, gameObject.transform);
             ship_shield.transform.localScale = new Vector2(shield_width, shield_height);
-            if(id == 1)//if this is player 2, switch the layer of the shield to player2shield
-            {
+
+            //
+            //  If this is Player 2,
+            //  switch the layer of the
+            //  shield to Player2Shield.
+            //
+            if(id == 1)
                 ship_shield.layer = 7;
-            }
-        }else if(shield_hp <= 0 && ship_shield != null)
+        }
+        else if(shield_hp <= 0 && ship_shield != null)
         {
             GameObject.Destroy(ship_shield);
         }
@@ -55,32 +92,94 @@ public class ShipScript : MonoBehaviour
     public void registerSelf()
     {
         if(ship_list == null)
-        {
             ship_list = new GameObject[1];
-        }
+
         if(ship_list.Length <= id)
         {
             GameObject[] longer_ship_list = new GameObject[id + 1];
             Array.Copy(ship_list, longer_ship_list, ship_list.Length);
             ship_list = longer_ship_list;
         }
+
         ship_list[id] = gameObject;
     }
 
     public void addRoomToShip(GameObject room)
     {
         int local_id = room.GetComponent<RoomScript>().getId();
+
         if (room_list == null)
-        {
             room_list = new GameObject[1];
-        }
+
         if(room_list.Length <= local_id)
         {
             GameObject[] longer_room_list = new GameObject[local_id + 1];
             Array.Copy(room_list, longer_room_list, room_list.Length);
             room_list = longer_room_list;
         }
+
         room_list[local_id] = room;
+
+        indexRoomUponAddition(room);
+    }
+
+    //
+    //  Whenever a new Room is 
+    //  added to the Ship, we want
+    //  to place it inside of its respective
+    //  "filtered" list.
+    //
+    //  TODO: Possibly research a method of
+    //        better handling this? Without
+    //        chaining conditionals, that is.
+    //
+    public void indexRoomUponAddition(GameObject room)
+    {
+        if (room == null)
+            return;
+
+        if (room.GetComponent<LaserRoomScript>() != null)
+        {
+            Debug.Log("--> indexRoomUponAddition(): INDEXED LASER ROOM.");
+            lasers.Add(room);
+        }
+        else if (room.GetComponent<HangarRoomScript>() != null)
+        {
+            Debug.Log("--> indexRoomUponAddition(): INDEXED HANGAR ROOM.");
+            hangars.Add(room);
+        }
+        else if (room.GetComponent<MissileRoomScript>() != null)
+        {
+            Debug.Log("--> indexRoomUponAddition(): INDEXED MISSILE ROOM.");
+            missiles.Add(room);
+        }
+        else if (room.GetComponent<EngineScript>() != null)
+        {
+            Debug.Log("--> indexRoomUponAddition(): INDEXED ENGINE ROOM.");
+            engines.Add(room);
+        }
+        else if (room.GetComponent<ShieldScript>() != null)
+        {
+            Debug.Log("--> indexRoomUponAddition(): INDEXED SHIELD ROOM.");
+            shields.Add(room);
+        }
+        else if (room.GetComponent<ReactorRoomScript>() != null)
+        {
+            Debug.Log("--> indexRoomUponAddition(): INDEXED REACTOR ROOM.");
+            reactors.Add(room);
+        }
+        else if (room.GetComponent<PointDefenseScript>() != null)
+        {
+            Debug.Log("--> indexRoomUponAddition(): INDEXED POINT DEFENSE ROOM.");
+            point_defenses.Add(
+                room
+            );
+        }
+        else
+        {
+            Debug.Log("--> indexRoomUponAddition(): !!! UNKNOWN ROOM !!!");
+            return;
+        }
     }
 
     public void registerCraft(GameObject craft) {
@@ -170,6 +269,11 @@ public class ShipScript : MonoBehaviour
             return ship_list[0];
         }
         return null;
+    }
+
+    public GameObject[] getRoomList()
+    {
+        return room_list;
     }
 
     public GameObject getRoomById(int id)
