0ef9bae0b2fcc621ffe438489fc2907833244698
kernel-dev <twitchisadeck@gmail.com>
2023-04-29 05:10:27 +0200

    update(EngineScript): comments & indentation

--- Assets/Scripts/RoomScripts/EngineScript.cs
+++ Assets/Scripts/RoomScripts/EngineScript.cs
@@ -6,7 +6,11 @@ public class EngineScript : RoomScript
 {
     [SerializeField] private float max_dodge;
     private float dodge;
-    // Start is called before the first frame update
+
+    //
+    //  Start is called before
+    //  the first frame update.
+    //
     void Start()
     {
         initRoom();
@@ -15,7 +19,10 @@ public class EngineScript : RoomScript
         owningShip.GetComponent<ShipScript>().recalculateDodge();
     }
 
-    // Update is called once per frame
+    //
+    //  Update is called
+    //  once per frame.
+    //
     void Update()
     {
         
@@ -30,17 +37,21 @@ public class EngineScript : RoomScript
     {
         hp -= Mathf.Abs(damage);
         float overflow;
+
         if (hp < 0)
         {
             overflow = hp; //save the dip into the negatives. We need this to assign hull damage
             hp = 0; //ensure that hp does not dip into negatives, otherwise bugs will occur
             owningShip.GetComponent<ShipScript>().TakeHullDamage(overflow);
         }
+
         int hp_floor = Mathf.FloorToInt(hp);
+
         for (int i = max_hp - 1; i >= hp_floor; i--)
         {
             room_health_display[i].GetComponent<SpriteRenderer>().color = Color.red;
         }
+
         dodge = max_dodge * max_hp / hp_floor;
         owningShip.GetComponent<ShipScript>().recalculateDodge();
     }
