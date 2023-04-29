ae521aefc9afdd0385089f782f18502f29fba326
kernel-dev <twitchisadeck@gmail.com>
2023-04-29 05:18:27 +0200

    update(ShieldScript): comments

--- Assets/Scripts/RoomScripts/ShieldScript.cs
+++ Assets/Scripts/RoomScripts/ShieldScript.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 
 public class ShieldScript : RoomScript
 {
-    // Start is called before the first frame update
+    //
+    //  Start is called before
+    //  the first frame update.
+    //
     void Start()
     {
         initRoom();
     }
 
-    // Update is called once per frame
+    //
+    //  Update is called
+    //  once per frame.
+    //
     void Update()
     {
         
