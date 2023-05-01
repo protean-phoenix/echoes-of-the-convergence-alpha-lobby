8f76335ba5fb38ae5b5f88ebadda3ee10a46298a
kernel-dev <twitchisadeck@gmail.com>
2023-05-02 01:40:23 +0200

    new(CorridorScript): beginnings

---
+++ Assets/Scripts/RoomConnectors/CorridorScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CorridorScript : MonoBehaviour
+{
+	//
+	//	The parent ship this corridor
+	//	is a part of.
+	//
+	[SerializeField] protected GameObject owningShip;
+
+	//
+	//	The two rooms this corridor
+	//	is connecting.
+	//
+	[SerializeField] protected (GameObject, GameObject) connectedRooms;
+
+
+	//
+	//	Start is called before
+	//	the initial frame update.
+	//
+	void Start()
+	{
+
+	}
+
+	//
+	//	Update is called
+	//	once per frame.
+	//
+	void Update()
+	{
+		
+	}
+}
+
